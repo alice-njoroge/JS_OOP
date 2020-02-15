fe8a8696853eca16339c95fdb121d4d6cb213849
Alice <alicenjoroge707@gmail.com>
2020-02-15 18:18:28 +0300

    open cart

--- app.js
+++ app.js
@@ -45,6 +45,8 @@ class Cart {
                 let productId = event.target.dataset.id;
                 let product = products.find(product => product.id === productId);
                 this.putProductInStorage(product);
+                this.openCart();
+
             });
         });
     }
@@ -55,12 +57,41 @@ class Cart {
             this.cartItems = JSON.parse(cartItems);
         }
 
-       if (!this.cartItems.find(item =>item.id === product.id)){
-           this.cartItems.push(product);
-           localStorage.setItem('cartItems', JSON.stringify(this.cartItems));
+        if (!this.cartItems.find(item => item.id === product.id)) {
+            this.cartItems.push(product);
+            localStorage.setItem('cartItems', JSON.stringify(this.cartItems));
 
-       }
+        }
 
+    }
+
+    /*cartUI() {
+        let cartItems = localStorage.getItem('cartItems');
+        this.cartItems = JSON.parse(cartItems);
+
+        let htmlCartString = '';
+        let cartString = this.cartItems.map(product => {
+            `<div class="cart-item">
+            <img src="${product.image}" alt="${product.title}">
+            <div>
+            <h4>${product.title} </h4>
+        <h5>${product.price}</h5>
+        <span class="remove-item">remove</span>
+            </div>
+            <div>
+            <i class="fas fa-chevron-up"></i>
+            <p class="item-amount">4</p>
+            <i class="fas fa-chevron-down"></i>
+            </div>
+            </div>`;
+            htmlCartString += cartString;
+        })
+
+    } */
+
+    openCart() {
+            document.querySelector('.cart').classList.add('showCart');
+            document.querySelector('.cart-overlay').classList.add('transparentBcg');
     }
 
 }
@@ -70,6 +101,8 @@ const cart = new Cart();
 productsObj.getProducts().then(products => {
     productsObj.displayProducts(products);
     cart.bagButtons(products);
+
+
 });
 
 
