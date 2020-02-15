a8d5e16172245732a26a21c4271615d206994dbf
Alice <alicenjoroge707@gmail.com>
2020-02-15 16:07:35 +0300

    restructure logic ie have products get and display methods in one class

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 //variables
 const productsDom = document.querySelector('.products-center');
 
-// products display
+
+
+// products class get them from json and display them on the UI
 class Products {
     async getProducts() {
         try {
             let result = await fetch('products.json');
             return await result.json();
 
+
         } catch (e) {
-            console.log(e);
+            return e;
         }
-
-
     }
 
-}
-
-class UI {
     displayProducts(products) {
         let productsHtmlString = '';
         products.map((product) => {
@@ -33,12 +31,31 @@ class UI {
             productsHtmlString += productString;
         });
         productsDom.innerHTML = productsHtmlString;
-
-
     }
+
+
 }
 
+class Cart {
+
+    bagButtons(products) {
+        let bagButtons = document.querySelectorAll('.bag-btn');
+        bagButtons.forEach((button) =>{
+            button.addEventListener('click', event=>{
+                let productId = event.target.dataset.id;
+                let product = products.find(product => product.id === productId);
+            });
+        } );
+
+    }
+
+}
+
+
 const productsObj = new Products();
-const ui = new UI;
-productsObj.getProducts().then(products => ui.displayProducts(products));
+const cart = new Cart();
+productsObj.getProducts().then(products => {
+    productsObj.displayProducts(products);
+});
+
 
